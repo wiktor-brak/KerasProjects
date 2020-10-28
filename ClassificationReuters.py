da7c6ec2c54d1f1365cb1daf5d56e6594f34b782
wiktor.brak <wiktor.brak@wp.pl>
2020-10-28 09:55:08 +0100

    Add Classification Reuters project

--- ClassificationReuters.py
+++ ClassificationReuters.py
@@ -1,56 +1,58 @@
-from keras.datasets import imdb
+from keras.datasets import reuters
 from keras import models
 from keras import layers
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
-print(train_data[0])
-word_index = imdb.get_word_index()
+(train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
+word_index = reuters.get_word_index()
 reverse_word_index = dict(
     [(value, key) for key, value in word_index.items()])
-decoded_review = ' '.join(
-    reverse_word_index.get(i - 3, '?') for i in train_data[0])
-print(decoded_review)
+decoded_data = ' '.join(
+    reverse_word_index.get(i - 3, '?') for i in train_data[44])
 
 
 def vectorize_sequences(sequences, dimension=10000):
     results = np.zeros((len(sequences), dimension))
     for i, sequence in enumerate(sequences):
-        results[i, sequence] = 1
+        results[i, sequence] = 1.
     return results
 
 
 x_train = vectorize_sequences(train_data)
 x_test = vectorize_sequences(test_data)
 
-y_train = np.asarray(train_labels).astype('float32')
-y_test = np.asarray(test_labels).astype('float32')
+
+def to_one_hot(labels, dimension=46):
+    results = np.zeros((len(labels), dimension))
+    for i, label in enumerate(labels):
+        results[i, label] = 1.
+    return results
+
+
+one_hot_train_labels = to_one_hot(train_labels)
+one_hot_test_labels = to_one_hot(test_labels)
 
 model = models.Sequential()
-model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(16, activation='relu'))
-model.add(layers.Dense(1, activation='sigmoid'))
-
-print(model.summary())
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
 
 model.compile(optimizer='rmsprop',
-              loss='binary_crossentropy',
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-x_val = x_train[:10000]
-partial_x_train = x_train[10000:]
+x_val = x_train[:1000]
+partial_x_train = x_train[1000:]
 
-y_val = y_train[:10000]
-partial_y_train = y_train[10000:]
+y_val = one_hot_train_labels[:1000]
+partial_y_train = one_hot_train_labels[1000:]
 
 history = model.fit(partial_x_train,
                     partial_y_train,
-                    epochs=20,
-                    batch_size=512,
-                    validation_data=(x_val, y_val))
-
-print(history.history.keys())
+                    epochs=9,
+                    batch_size=512,               validation_data=(x_val, y_val))
 
 acc = history.history['accuracy']
 vaL_acc = history.history['val_accuracy']
